ae03532ed5ceabb21ed243a752faa0043cb5e59e
Sijmen Schoon <sijmenschoon@gmail.com>
2016-01-22 22:36:06 +0100

    Add own modifications

--- gitlab2tele/__init__.py
+++ gitlab2tele/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import telebot
 import json
-
+import dateutil.parser
 
 class TeleSender():
     def __init__(self, token, chat_id):
@@ -18,15 +18,33 @@ class TeleSender():
 
     def __revice_push(self, json_obj):
         msg = self.__parse_push_event(json_obj)
-        self.bot.send_message(self.chat_id, msg)
+        self.bot.send_message(self.chat_id, msg, parse_mode='Markdown')
 
     def __parse_push_event(self, json_obj):
         push_user = json_obj['user_name']
         commit_count = json_obj['total_commits_count']
         repo_url = json_obj['repository']['homepage']
         repo_name = json_obj['repository']['name']
-        ret = '%s push %d commits to %s. %s' % (push_user, commit_count, repo_name, repo_url)
-        return ret
+        commits = json_obj['commits']
+
+        if commit_count == 1:
+            msg = '*%s pushed 1 commit ' % push_user
+        else:
+            msg = '*%s pushed %d commits ' % (push_user, commit_count)
+
+        if commit_count > 3:
+            msg += 'to %s (showing last 3):*\n' % repo_name
+        else:
+            msg += 'to %s:*\n' % repo_name
+
+        for commit in commits[-3:]:
+            dt = dateutil.parser.parse(commit['timestamp']).replace(tzinfo=None)
+            pd = pretty_date(dt)
+
+            commit_message = commit['message'].rstrip()
+            msg += '_%s (%s)_\n``` %s ```\n' % (commit['author']['name'], pd, commit_message)
+
+        return msg
 
     def __revice_merge_request(self, json_obj):
         msg = self.__parse_merge_request(json_obj)
@@ -39,3 +57,48 @@ class TeleSender():
         state = json_obj['object_attributes']['state']
         ret = u'%s %s a merge request : %s. %s' % (request_user, state, mr_title, url)
         return ret
+
+def pretty_date(time=False):
+    """
+    Get a datetime object or a int() Epoch timestamp and return a
+    pretty string like 'an hour ago', 'Yesterday', '3 months ago',
+    'just now', etc.
+
+    Nicked from https://stackoverflow.com/a/1551394.
+    """
+    from datetime import datetime
+    now = datetime.now()
+    if type(time) is int:
+        diff = now - datetime.fromtimestamp(time)
+    elif isinstance(time,datetime):
+        diff = now - time
+    elif not time:
+        diff = now - now
+    second_diff = diff.seconds
+    day_diff = diff.days
+
+    if day_diff < 0:
+        return ''
+
+    if day_diff == 0:
+        if second_diff < 10:
+            return "just now"
+        if second_diff < 60:
+            return str(second_diff) + " seconds ago"
+        if second_diff < 120:
+            return "a minute ago"
+        if second_diff < 3600:
+            return str(second_diff // 60) + " minutes ago"
+        if second_diff < 7200:
+            return "an hour ago"
+        if second_diff < 86400:
+            return str(second_diff // 3600) + " hours ago"
+    if day_diff == 1:
+        return "Yesterday"
+    if day_diff < 7:
+        return str(day_diff) + " days ago"
+    if day_diff < 31:
+        return str(day_diff // 7) + " weeks ago"
+    if day_diff < 365:
+        return str(day_diff // 30) + " months ago"
+    return str(day_diff // 365) + " years ago"
--- runserver.py
+++ runserver.py
@@ -1,5 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+#
+# Telegram bot using Flask that acts like a GitLab webhook to
+# share the commits of a GitLab project to a Telegram chat.
+#
+# Copyright (c) 2015 eternnoir
+# Modified by Sijmen Schoon
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
 import os
 from flask import Flask, request
 import gitlab2tele
